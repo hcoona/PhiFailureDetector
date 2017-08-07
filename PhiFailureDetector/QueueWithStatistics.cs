fb621efe890e612ad922e7968809d1e48006d992
Shuai Zhang <zhangshuai.ustc@gmail.com>
2017-08-07 18:26:38 +0800

    Add projects.

---
+++ PhiFailureDetector/QueueWithStatistics.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PhiFailureDetector
+{
+    public class QueueWithStatistics<T>
+    {
+        private readonly Queue<T> m_queue;
+    }
+}
